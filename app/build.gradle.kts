455ea082da9aa15e28e9a054e0c2ce09693581de
sinatoe <219674570+sinatoe@users.noreply.github.com>
2026-06-18 13:22:54 +0200

    chore: update dependencies and migrate to new build options

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -39,13 +39,9 @@ android {
         }
 
         release {
-            isMinifyEnabled = true
-            isShrinkResources = true
-
-            proguardFiles(
-                getDefaultProguardFile("proguard-android-optimize.txt"),
-                "proguard-rules.pro",
-            )
+            optimization {
+                enable = true
+            }
 
             signingConfig = signingConfigs.getByName("release")
         }
