266b45efd47b0b3e7793a67663392f8a0076b2e1
sinatoe <219674570+sinatoe@users.noreply.github.com>
2026-08-20 08:55:40 +0200

    refactor: use factoryOf DSL in ClickerModule

--- app/src/main/java/io/github/sinatoe/slideclick/di/ClickerModule.kt
+++ app/src/main/java/io/github/sinatoe/slideclick/di/ClickerModule.kt
@@ -2,11 +2,11 @@ package io.github.sinatoe.slideclick.di
 
 import io.github.sinatoe.slideclick.data.ClickerService
 import io.github.sinatoe.slideclick.ui.ClickerViewModel
-import org.koin.android.ext.koin.androidContext
+import org.koin.core.module.dsl.factoryOf
 import org.koin.core.module.dsl.viewModelOf
 import org.koin.dsl.module
 
 val clickerModule = module {
-    factory { ClickerService.connectionFlow(androidContext()) }
+    factoryOf(ClickerService::connectionFlow)
     viewModelOf(::ClickerViewModel)
 }
