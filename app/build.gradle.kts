b793526997cb745fdf9075e39f33141a68759882
sinatoe <219674570+sinatoe@users.noreply.github.com>
2026-08-23 19:32:29 +0200

    chore: bump version to 0.1.2

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -14,8 +14,8 @@ android {
         applicationId = "io.github.sinatoe.slideclick"
         minSdk = 28
         targetSdk = 37
-        versionCode = 2
-        versionName = "0.1.1"
+        versionCode = 3
+        versionName = "0.1.2"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
